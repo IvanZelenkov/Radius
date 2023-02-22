af2256c95c7be58702674d8900fdec9d40f48368
IvanZelenkov <itproger.ivan@gmail.com>
2023-02-21 18:36:12 -0600

    Completed front-end Dockerfile, and still working on back-end Dockerfile.

--- radius-chat-client/src/constants/constants.js
+++ radius-chat-client/src/constants/constants.js
@@ -1,2 +1,2 @@
-export const SOCKET_BASE_URL = "http://192.168.1.65:8085";
-export const API_BASE_URL = "http://192.168.1.65:8080";
+export const SOCKET_BASE_URL = "http://172.24.0.1:8085";
+export const API_BASE_URL = "http://172.24.0.1:8080";
