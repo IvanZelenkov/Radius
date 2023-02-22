8d72d2466aaf295ddc2374e36b8037a62eb42258
IvanZelenkov <itproger.ivan@gmail.com>
2023-02-22 15:52:25 -0600

    Problem connecting to 0.0.0.0:8080 from client

--- radius-chat-client/src/constants/constants.js
+++ radius-chat-client/src/constants/constants.js
@@ -1,2 +1,2 @@
-export const SOCKET_BASE_URL = "http://172.24.0.1:8085";
-export const API_BASE_URL = "http://172.24.0.1:8080";
+export const SOCKET_BASE_URL = "http://0.0.0.0:8085";
+export const API_BASE_URL = "http://0.0.0.0:8080";
